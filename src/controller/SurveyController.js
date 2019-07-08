c36f5400063c6a196782e67738e6efdcb015c0b0
Leonardo Gerheim de Andrade <leonardo.g.andrade@gmail.com>
2019-07-08 17:40:12 -0300

    configuration

--- src/controller/SurveyController.js
+++ src/controller/SurveyController.js
@@ -3,7 +3,7 @@ const Survey = mongoose.model('Survey');
 
 module.exports = {
     async voteAgree(req,res){
-        const id = '5d234f711ed5b94015c2e98e';
+        const id = '5d23a90e1924a2b756f29489';
         const vote = await Survey.findById(id);
         const { agree, disagree } = req.body;
         vote.agree += parseInt(agree);
